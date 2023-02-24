8517b316da6961827e21167f01c777edba406d25
raiden00pl <raiden00@railab.me>
2023-02-24 13:26:31 +0100

    tests: mock MplManager.show for session

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,25 @@
+from unittest import mock
+
 import matplotlib
+import pytest  # type: ignore
+
+import nxscli
 
 
 def pytest_sessionstart(session):
     # force no TK gui
     matplotlib.use("Agg")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def default_session_fixture(request):
+    # mock MplManager show method
+    patched = mock.patch.object(
+        nxscli.plot_mpl.MplManager, "show", autospec=True
+    )
+    patched.start()
+
+    def unpatch():
+        patched.stop
+
+    request.addfinalizer(unpatch)
