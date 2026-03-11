4087f04c2cd89b1c35e775fc8194a02eb9507e6a
raiden00pl <raiden00@railab.me>
2026-03-11 10:30:18 +0100

    improve devinfo tests
    Signed-off-by: raiden00pl <raiden00@railab.me>

--- tests/plugins/test_devinfo.py
+++ tests/plugins/test_devinfo.py
@@ -1,3 +1,9 @@
+from nxslib.intf.dummy import DummyDev
+from nxslib.nxscope import NxscopeHandler
+from nxslib.proto.parse import Parser
+
+from nxscli.iplugin import DPluginDescription
+from nxscli.phandler import PluginHandler
 from nxscli.plugins.devinfo import PluginDevinfo
 
 
@@ -7,3 +13,26 @@ def test_plugindevinfo_init():
     assert plugin.stream is False
 
     # TODO:
+
+
+def test_plugindevinfo_content():
+    intf = DummyDev()
+    parse = Parser()
+    nxscope = NxscopeHandler(intf, parse, enable_bitrate_tracking=True)
+
+    phandler = PluginHandler([DPluginDescription("pdevinfo", PluginDevinfo)])
+    phandler.nxscope_connect(nxscope)
+
+    plugin = PluginDevinfo()
+    plugin.connect_phandler(phandler)
+
+    assert plugin.start({}) is True
+    out = plugin.result()
+
+    assert "Device common" in out
+    assert "Channels state (applied)" in out
+    assert "Channels state (buffered)" in out
+    assert "stream_started" in out
+    assert "bitrate" in out
+
+    phandler.cleanup()
